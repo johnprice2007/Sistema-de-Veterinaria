fb9faaf69b92cdcc56f97bcc35892ff11b94960c
johnprice2007 <leandercustodiocollas2007@gmail.com>
2026-06-07 20:05:15 -0500

    Creación clase dueño

--- src/sistema/de/veterinaria/SistemaDeVeterinaria.java
+++ src/sistema/de/veterinaria/SistemaDeVeterinaria.java
@@ -4,7 +4,13 @@ package sistema.de.veterinaria;
 public class SistemaDeVeterinaria {
 
     public static void main(String[] args) {
-        java.awt.EventQueue.invokeLater(() -> new InterfazDeSistema().setVisible(true));
-        
+        java.awt.EventQueue.invokeLater(() -> {
+    javax.swing.JFrame ventana = new javax.swing.JFrame("Login");
+    ventana.setDefaultCloseOperation(javax.swing.JFrame.EXIT_ON_CLOSE);
+    ventana.setContentPane(new Login());
+    ventana.pack();
+    ventana.setLocationRelativeTo(null);
+    ventana.setVisible(true);
+});
     }
 }
